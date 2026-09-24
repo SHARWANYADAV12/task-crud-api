564adc4a294867372b04e1f080b92e1f83ad57d2
SHARWAN YADAV <sharwanyadav@Sharwans-MacBook-Air.local>
2026-09-24 12:48:21 +0530

    Stage 5: Swagger UI

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./openapi.json");
 
 const app = express();
 const PORT = 3000;
@@ -132,6 +134,9 @@ app.delete("/tasks/:id", (req, res) => {
 
     res.status(204).send();
 });
+
+// Swagger UI
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Start server
 app.listen(PORT, () => {
